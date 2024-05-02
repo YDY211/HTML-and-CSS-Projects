9b32bd2dbb33e2f8bd7f611782ad88cf17aa92af
YDY211 <134720367+YDY211@users.noreply.github.com>
2024-05-02 12:46:17 +0100

    assignemnt

---
+++ Basic_HTML_and_Javascript_1/Basic_JavaScript_1.js
@@ -0,0 +1,10 @@
+<script src=“Basic_JavaScript_1.js”></script>
+
+<script>
+Function My_First_Function () {
+var String = “Kiss me, I’m Irish”;
+var result = String.fontcolor(“green) ;
+Document.getElementById(“Irish”).innerHTML =
+result;
+}
+</script
