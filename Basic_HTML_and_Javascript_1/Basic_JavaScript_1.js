493b5581832546cf1ea930880df3fc11e9314ee7
YDY211 <134720367+YDY211@users.noreply.github.com>
2024-05-04 18:42:27 +0100

    basic html 5

--- Basic_HTML_and_Javascript_1/Basic_JavaScript_1.js
+++ Basic_HTML_and_Javascript_1/Basic_JavaScript_1.js
@@ -1,10 +1,8 @@
-<script src=“Basic_JavaScript_1.js”></script>
-
-<script>
-Function My_First_Function () {
-var String = “Kiss me, I’m Irish”;
-var result = String.fontcolor(“green) ;
-Document.getElementById(“Irish”).innerHTML =
-result;
-}
-</script
+<><script src Basic_JavaScript_1 js></script><script>
+    Function My_First_Function () { 
+    var String = Kiss me, Im Irish;
+    var result = String.fontcolor(green) ;
+    Document.getElementByIdIrish).innerHTML =
+    result;
+    }
+</script></>
